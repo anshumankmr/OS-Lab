31b85700a36822a7a1d8c4092b53f05246a014d7
Anshuman <anshumankmr80@gmail.com>
2018-11-01 16:25:32 +0530

    Added Question 2 of Week 12

--- week12/q13.c
+++ week12/q13.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-int flag=0;
 void copy(int arr1[], int arr2[], int num)
 {
 	int i;
@@ -9,6 +8,12 @@ void copy(int arr1[], int arr2[], int num)
    {
       arr2[i] = arr1[i];
    }
+}
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
 }
 int getMax(int arr[], int n)
 {
@@ -38,24 +43,6 @@ int getMin(int arr[], int n)
 	}
 	return min;
 }
-void fcfs(int array[], int header, int n)
-{
-	printf("%d->",header );
-	long mvmt=0;
-	for (int i =0; i<n;i++)
-	{
-     printf("%d->",array[i] );
-     mvmt+=abs(array[i]-header);
-     header=array[i];
-	}
-	printf("\nTotal movement of header %ld\n",mvmt );
-}
-void swap(int *xp, int *yp) 
-{ 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
 void bubbleSort(int arr[],int pos[], int n) 
 { 
    int i, j; 
@@ -71,67 +58,10 @@ void bubbleSort(int arr[],int pos[], int n)
         }
      }      	  
 } 
-int zerocount(int diff[], int n)
+void ScanDISK(int Array[], int header, int n,int lb, int ub)
 {
-	int zc=-1,i;
-	for (  i =0 ;i < n ; i++)
-	{
-		if (diff[i]!=0 && zc == -1) 
-		{
-			return i;
-		}
-		if (zc==-1 && diff[i]==0)
-		{
-         zc+=1;
-		}
-		if (zc > -1 && diff[i]==0 && flag > 0 && i!=0 )
-		{
-			flag+=1;
-			return flag;
-		}
-		if (zc>-1 && diff[i]!=0)
-		{
-			return i;
-		}
-	}
-}
-int getShortest(int array[], int header, int n)
-{
-	int diff[n], pos[n];
-	for (int i=0;i<n;i++)
-	{
-		pos[i]=i;
-	}
-	for (int i =0 ;i<n; i++)
-	{
-		diff[i]=abs(array[i]-header);
-		//printf("\n diff %d %d ",i , diff[i] );
-	}
-	bubbleSort(diff,pos,n);
-    return pos[zerocount(diff,n)];
-    
-}
-void srtf(int Array[], int header, int n)
-{
-	printf("%d->",header );
-	long mvmt=0;
-	int array[n];
-	copy(Array,array,n);
-	for (int i =0; i<n;i++)
-    {
-	 int pos = getShortest(array,header,n);
-     printf("%d->",array[pos] );
-     mvmt+=abs(array[pos]-header);
-     header=array[pos];
-     array[pos]=INT_MAX;
-	}
-	printf("\nTotal movement of header %ld\n",mvmt );
-}
-void SanDisk(int Array[], int header, int n,int lb, int ub)
-{
-	printf("in here\n");
+	// printf("in here\n");
 	printf("%d ->",header);
-	
 	int temp=n;
 	int j=0;
 	long mvmt=0;
@@ -196,13 +126,11 @@ int main()
 	}
     printf("Enter the header\n");
     scanf("%d",&header);
-	fcfs(array,header,n);
-	srtf(array,header,n);
 	printf("Enter the range of cylinders \n");
 	int lb,ub;
 	scanf("%d ",&lb);
 	scanf("%d",&ub);
-	printf("%d ",header);
-	int header1=header;
-	SanDisk(array,header1,n,0,499);
+	printf("Header %d ",header);
+	///int header1=header;
+	ScanDISK(array,header,n,0,499);
 }
