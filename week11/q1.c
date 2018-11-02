d50d7ff78593a80671b789259647a73374251c56
Anshuman <anshumankmr80@gmail.com>
2018-11-02 13:10:39 +0530

    fixed mistake in question 1 of week1 1

--- week11/q1.c
+++ week11/q1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// Modules independent of Algorithm
 int j;
 int positionsareempty(int arr[], int frame)
 {
@@ -15,6 +16,16 @@ int positionsareempty(int arr[], int frame)
     }
     return ret;
 }
+void printdist(int arr[], int frame)
+{
+    printf("Frame Table\n");
+    for (int i =0;i<frame;i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+//can use this for verification
+
 int hit(int page, int arr[], int size)
 {
     for (int i =0 ;i<size;i++)
@@ -28,45 +39,57 @@ int hit(int page, int arr[], int size)
          return i;
         }
     }
-    return 0;
+    return -1;
 }//can improve with binary search 
 void append(int arr[], int frame, int value)
 {
     arr[j]=value;
     j=(j+1)%frame;
 }
+// 
+// FIFO Page Replacement
+//
+//
 void fifoPageRep(int str[], int lstr, int frame )
 {
     int arr[frame];
-    int pf=0;
+    int pf=0,ph=0;
     memset(arr, -1 , frame*sizeof(int));
     for (int i =0 ;i<lstr;i++)
     {
-        if (hit(str[i],arr,frame))
+        if (hit(str[i],arr,frame)>=0)
         {
          //do nothing
+            ph+=1;
         }
         else
         {
             append(arr,frame,str[i]);
+            //printdist(arr,frame);
             pf+=1;
         }
     }
+    printf("Page Hits For FIFO Page Replacement Algorithm %d\n",ph );
+
     printf("Page Fault For FIFO Page Replacement Algorithm %d\n",pf );
 }
+//
+//
 // Corresponding Code for Optimal Page Rep
+//
+//
 int getFirstOccurrence(int page, int str[], int lstr, int position)
 {
-    for (int i = position+1 ; i< lstr;i++)
+    for (int i = position ; i< lstr;i++)
     {
         if (str[i]==page)
         {
-            return (i);
+            return i;
         }
     }
-    return (lstr-1);
+    return lstr;
 }
-int getDistance(int dist[],int arr[],int str[], int page, int lstr,int frame, int position)
+int getDistance(int dist[],int arr[],int str[],  int lstr,int frame, int position)
 {
     for (int i =0; i< frame ;i++)
     {
@@ -86,42 +109,34 @@ int getMaxPos(int arr[], int frame)
     }
     return maxpos;
 }
-int getMinPos(int arr[], int frame)
-{
-    int min=arr[0],minpos=0;
-    for (int i =1 ;i< frame;i++)
-    {
-        if (arr[i]<min)
-        {
-            min =arr[i];
-            minpos=i;
-        }
-    }
-    return minpos;
-}
-// void printdist(int arr[], int frame)
+// int getMinPos(int arr[], int frame)
 // {
-//     printf("Frame Table\n");
-//     for (int i =0;i<frame;i++)
+//     int min=arr[0],minpos=0;
+//     for (int i =1 ;i< frame;i++)
 //     {
-//         printf("%d ", arr[i]);
+//         if (arr[i]<min)
+//         {
+//             min =arr[i];
+//             minpos=i;
+//         }
 //     }
+//     return minpos;
 // }
-// //can use this for verification
 void  optimalPageRep(int str[], int lstr, int frame )
 {
     int arr[frame];
-    int pf=0;
+    int pf=0,ph=0;
     memset(arr, -1 , frame*sizeof(int));
     for (int i =0 ;i<lstr;i++)
     {
-        if (hit(str[i],arr,frame))
+        if (hit(str[i],arr,frame)>=0)
         {
-         //do nothing
+         
+            ph+=1;
         }//from here onwards we are only dealing with miss cases so appending is fine
         else 
         {
-           if (i>=0  && i<= frame-1)//need to fix for certain other use cases potential fix positionsareempty(arr,frame)?
+           if (positionsareempty(arr,frame))
           {
             append(arr,frame,str[i]);
             pf+=1;
@@ -131,8 +146,7 @@ void  optimalPageRep(int str[], int lstr, int frame )
           else 
          {
           int dist[frame];
-          int page=str[i];
-          getDistance(dist,arr,str,page,lstr,frame,i);
+          getDistance(dist,arr,str,lstr,frame,i);
           arr[getMaxPos(dist,frame)]=str[i];
           pf+=1;
           // printf("\npage faulted %d  for %d \n",pf,str[i]);
@@ -140,39 +154,60 @@ void  optimalPageRep(int str[], int lstr, int frame )
          }
         }
     }
+
+    printf("\nPage Hits For Optimal Page Page Replacement Algorithm %d\n",ph);
+   
     printf("Page Fault For Optimal Page Replacement Algorithm %d\n",pf );
 }
+//
 // Least Recently Used Algo
+//
+int getOccurrence(int page, int str[], int lstr, int position)
+{
+    for (int i = position ; i>=0;i--)
+    {
+        if (str[i]==page)
+        {
+            return i;
+        }
+    }
+}
+int getRevDistance(int dist[],int arr[],int str[],  int lstr,int frame, int position)
+{
+    for (int i =0; i< frame ;i++)
+    {
+        dist[i] =abs(getOccurrence(arr[i],str,lstr,position) - position);
+    }
+}
 void  lruPageRep(int str[], int lstr, int frame )
 {
-    int arr[frame], count[frame];
-    int pf=0;
+    int arr[frame];
+    int pf=0,ph=0;
     memset(arr, -1 , frame*sizeof(int));
-    memset(count, 0 , frame*sizeof(int));
     for (int i =0 ;i<lstr;i++)
     {
         int temp=hit(str[i],arr,frame);
-        if (temp)
+        if (temp>=0)
         {
-         count[temp]+=1;
+         ph+=1;
         }
         else 
         {
            if (positionsareempty(arr,frame))
           {
-            count[j]+=1;
             append(arr,frame,str[i]);
             pf+=1;
           }
           else 
          {
-          int pos =getMinPos(count,frame);
-           arr[pos]=str[i];
-           count[pos]=0;
-           pf+=1;
+          int dist[frame];
+          getRevDistance(dist,arr,str,lstr,frame,i);
+          arr[getMaxPos(dist,frame)]=str[i];
+          pf+=1;
          }
         }
     }
+    printf("\nPage Hits For Least Recently Used Page Replacement Algorithm %d\n",ph);
     printf("Page Fault For Least Recently Used Page Replacement Algorithm %d\n",pf );
 }
 int main()
